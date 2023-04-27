bde90a80e19d9ff25b4b09dce7ffa82b1d8a15a3
muhammadsufian <muhammadsufian744@gmail.com>
2023-04-27 14:48:55 +0500

    Hostfully QA Engineer

---
+++ cypress/e2e/adding_a_new_computer.cy.js
@@ -0,0 +1,28 @@
+describe('Adding a new computer', () => {
+
+  beforeEach(() => {
+    // Run this code before each test
+  cy.visit('https://computer-database.gatling.io/computers');
+  cy.wait(2000)
+  });
+
+  it('Adds a new computer with valid data', () => {
+    cy.get('#add').click()
+    cy.get('#name').type('New Computer')
+    cy.get('#introduced').type('2022-01-01')
+    cy.get('#discontinued').type('2023-01-01')
+    cy.get('#company').select('Apple Inc.')
+
+    cy.get('.actions > [type="submit"]').click()
+    cy.wait(10000)
+    cy.contains('Done ! Computer New Computer has been created')
+  })
+
+  it('Displays error message when adding a new computer with invalid data', () => {
+    cy.get('#add').click()
+
+    cy.get('.actions > [type="submit"]').click()
+
+    cy.contains('Computer name')
+  })
+})
